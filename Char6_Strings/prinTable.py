ccc09c14c5f6fa7babefb4ec8e116954bd83a4ea
a.gorgoma@securitycode.ru <a.gorgoma@securitycode.ru>
2015-11-03 21:13:43 +0300

    xz

--- Char6_Strings/prinTable.py
+++ Char6_Strings/prinTable.py
@@ -19,12 +19,21 @@ def longest(list):
             continue
     return max_long
 
-#print(longest(tableData[0])
+
+#colWidths = [0] * len(tableData[0])
+#print(colWidths)
 
 
-colWidths = [0] * len(tableData[0])
-print(colWidths)
+# example like Char4_picGrid
+#print(tableData[0][0], tableData[1][0], tableData[2][0])
+#print(tableData[0][1], tableData[1][1], tableData[2][1])
 
+'''
+for y in range(len(tableData[0])):
+    a = ''
+    for x in range(len(tableData)):
+        a += tableData[x][y].rjust(8, '*')
+    print(a)
+'''
 
-
-print(tableData[0][0], tableData[1][0], tableData[2][0])
+print(longest(tableData))
