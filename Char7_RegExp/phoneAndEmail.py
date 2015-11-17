3b11a811a3dd7412320cfea36241d15185df9407
a.gorgoma@securitycode.ru <a.gorgoma@securitycode.ru>
2015-11-17 13:14:23 +0300

    phoneAndEmail - done

--- Char7_RegExp/phoneAndEmail.py
+++ Char7_RegExp/phoneAndEmail.py
@@ -16,7 +16,7 @@ phoneRegex = re.compile(r'''(
 emailRegex = re.compile(r'''(
     [a-zA-Z0-9._%+-]+    # username
     @                    # @ symbol
-    [a-zA-Z0-9.-]        # domain name
+    [a-zA-Z0-9.-]+       # domain name
     (\.[a-zA-Z]{2,4})    # dot-something
 )''', re.VERBOSE)
 
