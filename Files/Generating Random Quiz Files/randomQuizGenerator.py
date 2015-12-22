9963a374e486f88ce0be52e17b032dfd13dbcaf9
a.gorgoma@securitycode.ru <a.gorgoma@securitycode.ru>
2015-12-22 21:01:17 +0300

    Write the question and answer options to the quiz file

--- Files/Generating Random Quiz Files/randomQuizGenerator.py
+++ Files/Generating Random Quiz Files/randomQuizGenerator.py
@@ -47,5 +47,12 @@ for quizNum in range(35):
         wrongAnswers = random.sample(wrongAnswers, 3)
         answerOptions = wrongAnswers + [correctAnswer]
         random.shuffle(answerOptions)
-    # TODO: Write the question and answer options to the quiz file.
+    # Write the question and answer options to the quiz file.
+    quizFile.write('%s. What is the capital of %s?\n' % (questionNum + states[questionNum]))
+    for i in range(4):
+        quizFile.write(' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
+    quizFile.write('\n')
     # TODO: Write the answer key to a file.
+    answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+    quizFile.close()
+    answerKeyFile.close()
