38ee076ed0343080a93a638d39e26f72a61b8902
a.gorgoma@securitycode.ru <a.gorgoma@securitycode.ru>
2015-12-24 20:06:05 +0300

    Files - need to do other task later

---
+++ Files/MadLibs/MadLib.py
@@ -0,0 +1,19 @@
+# TODO нужно доделать упражнение
+
+# Enter words to replace
+adjective = input('Enter an adjective: ')
+
+noun1 = input('Enter a noun: ')
+
+verb = input('Enter a verb: ')
+
+noun2 = input('Enter a noun: ')
+
+# Find and replace
+textFile = open('text.txt', 'r')
+
+
+textFile.close()
+
+
+
---
+++ Files/Regex_Search/RegSearch.py
@@ -0,0 +1,7 @@
+# TODO нужно доделать упражнение
+
+"""
+Write a program that opens all .txt files in a folder and
+    searches for any line that matches a user-supplied regular expression.
+The results should be printed to the screen.
+"""
